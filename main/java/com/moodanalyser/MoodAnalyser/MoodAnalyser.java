568967564e8877689c7a20da69e4c228779111b2
ShivKokkula <shivbk987@gmail.com>
2021-05-29 23:14:07 +0530

    Refactor - refactored to accomodate Test case 1.1 and 1.2

--- main/java/com/moodanalyser/MoodAnalyser/MoodAnalyser.java
+++ main/java/com/moodanalyser/MoodAnalyser/MoodAnalyser.java
@@ -1,11 +1,19 @@
 package com.moodanalyser.MoodAnalyser;
 
 public class MoodAnalyser {
-    public String analyseMood(String message) {
-        String messageToBeAnalysed = message;
+    public static String messageToBeAnalysed;
+    public String analyseMood() {
         if(messageToBeAnalysed.contains("sad"))
             return "SAD";
         else
             return "HAPPY";
+    }
+
+    public MoodAnalyser(String message) {
+        this.messageToBeAnalysed = message;
+    }
+
+    public MoodAnalyser() {
+        this.messageToBeAnalysed = "sad";
     }
 }
--- test/java/MoodAnalyserTest.java
+++ test/java/MoodAnalyserTest.java
@@ -7,15 +7,32 @@ import java.util.Scanner;
 public class MoodAnalyserTest {
     @Test
     void givenSadMessage_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String sadMessage = moodAnalyser.analyseMood("I am in sad mood");
-        Assertions.assertEquals("SAD",sadMessage);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        Assertions.assertEquals("SAD",moodAnalyser.analyseMood());
     }
-
     @Test
     void givenHappyMessage_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any mood");
+        Assertions.assertEquals("HAPPY",moodAnalyser.analyseMood());
+    }
+    @Test
+    void givenSadMessage_WithParameter_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        Assertions.assertEquals("SAD",moodAnalyser.analyseMood());
+    }
+    @Test
+    void givenSadMessage_WithoutParameter_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String happyMessage = moodAnalyser.analyseMood("I am in Any mood");
-        Assertions.assertEquals("HAPPY",happyMessage);
+        Assertions.assertEquals("SAD",moodAnalyser.analyseMood());
+    }
+    @Test
+    void givenHappyMessage_WithParameter_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
+        Assertions.assertEquals("HAPPY",moodAnalyser.analyseMood());
+    }
+    @Test
+    void givenHappyMessage_WithoutParameter_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        Assertions.assertEquals("HAPPY",moodAnalyser.analyseMood());
     }
 }
